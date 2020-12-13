a8dd411211be0b8ddd6053861e08ade9c4839506
ftocchetto <felipe@tocchetto.com>
2020-12-12 21:09:51 -0300

    separado em arquivos por tópicos

--- hello.py
+++ hello.py
@@ -1,6 +1,2 @@
 print("hello world")
 print(2-1)
-name = "felipe"
-print (name)
-surname = "tocchetto"
-print (name + " "+ surname)
---
+++ variables.py
@@ -0,0 +1,19 @@
+name = "felipe"
+print(name)
+surname = "tocchetto"
+print(name + " "+ surname)
+mypets = "2"
+print(type(mypets))
+print("I have " + mypets + " dogs")
+mypets = ["Pablo", "Carmen"]
+print(type(mypets))
+print(mypets)
+a = 10
+print(a)
+a = a + a
+print(a)
+print(type(a))
+income = 1000
+taxrate = 0.10
+taxes = income * taxrate
+print (taxes)
